11d32115e1c5271884a0cd10d9042b68629fb9b9
1badnews <aras.butrimanskas@gmail.com>
2023-01-03 18:37:01 +0200

    cookies

--- confusionserver/app.js
+++ confusionserver/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('./express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -29,13 +31,23 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 function auth(req,res,next)
 {
-  console.log(req.headers);
+  console.log(req.signedCookies);
 
-  var authHeader = req.headers.authorization;
+  if (!req.signedCookies.user)
+  {
+    var authHeader = req.headers.authorization;
 
   if (!authHeader)
   {
@@ -45,13 +57,14 @@ function auth(req,res,next)
     return next(err);
   }
 
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':'); // make array of strings from headers
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':'); // make array of strings from headers
 
   var username = auth[0];
   var password = auth[1];
 
   if(username === 'admin' && password === 'password')
   {
+    res.cookie('user', 'admin', {signed: true})
     next();
   }
   else 
@@ -61,6 +74,22 @@ function auth(req,res,next)
     err.status = 401;
     return next(err);
   }
+  }
+  else
+  {
+    if (req.signedCookies.user === 'admin')
+    {
+      next();
+    }
+  
+  else
+  {
+    var err = new Error('Unauthorized');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+}
 }
 
 app.use(auth); // authenticate users
