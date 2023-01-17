ee69d240cead06cb5bc373d4283d59b7de3107f4
1badnews <aras.butrimanskas@gmail.com>
2023-01-17 02:55:06 +0200

    assignment 3 1/3

--- confusionserver/authenticate.js
+++ confusionserver/authenticate.js
@@ -38,3 +38,11 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => 
 }));
 
 exports.verifyUser = passport.authenticate('jwt', {session:false});
+
+exports.verifyAdmin = (req, res, next) =>{
+        if(req.user.admin){
+            next();
+        }else{
+            res.status(403).send("Unauthorized");
+        }
+    }
--- confusionserver/routes/leaderRouter.js
+++ confusionserver/routes/leaderRouter.js
@@ -17,7 +17,7 @@ leaderRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post(authenticate.verifyUser,(req,res,next) => {
+.post(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next) => {
     Leaders.create(req.body)
     .then((leaders) =>
     {
@@ -28,11 +28,11 @@ leaderRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.put(authenticate.verifyUser,(req,res,next) => {
+.put(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /promotions');
 })
-.delete(authenticate.verifyUser,(req,res,next) => {
+.delete(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next) => {
     Leaders.remove({})
     .then ((leaders) => {
         res.statusCode = 200
@@ -57,11 +57,11 @@ leaderRouter.route('/:leadersId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post(authenticate.verifyUser,(req,res,next) => {
+.post(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next) => {
     res.statusCode=403;
     res.end('Post operation not supported on /promotions/?');
 })
-.put(authenticate.verifyUser,(req,res,next) => {
+.put(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next) => {
     Leaders.findByIdAndUpdate(req.params.leadersId, {
         $set: req.body
     }, {new: true})
@@ -72,7 +72,7 @@ leaderRouter.route('/:leadersId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.delete(authenticate.verifyUser,(req,res,next) => {
+.delete(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next) => {
     Leaders.findByIdAndRemove(req.params.leadersId)
     .then ((leaders) => {
         res.statusCode = 200
